d1b804591438763eff1280b1abfec22f3b91a799
abdelazizben <abdelazizben13@gmail.com>
2022-12-14 23:11:22 +0100

    Ajout menu interactif

--- gouts/gouts.js
+++ gouts/gouts.js
@@ -8,3 +8,37 @@ window.addEventListener('load', () => {
 
     TL.play();
 })
+var tab = document.querySelectorAll(".card");
+var fleche1 = document.querySelector(".img-svg1");
+var fleche2 = document.querySelector(".img-svg2");
+var slide = document.querySelector(".slide");
+var indice_carte_milieu = 1;
+function augmenter(variable) {
+    variable.style = "z-index:2;transform: scale(1.2);width:calc(2*100%);";
+}
+function diminuer(variable) {
+    variable.style = "transform: scale(1); z-index=1";
+
+}
+
+window.onload = () => {
+    augmenter(tab[indice_carte_milieu]);
+}
+
+fleche1.onclick = () => {
+    if (indice_carte_milieu != 0) {
+        diminuer(tab[indice_carte_milieu]);
+        augmenter(tab[indice_carte_milieu - 1]);
+        slide.scrollBy(-210, 0);
+        indice_carte_milieu = indice_carte_milieu - 1;
+    }
+}
+
+fleche2.onclick = () => {
+    if (indice_carte_milieu != 3) {
+
+        diminuer(tab[indice_carte_milieu]);
+        augmenter(tab[indice_carte_milieu + 1]);
+        slide.scrollBy(210, 0);
+        indice_carte_milieu = indice_carte_milieu + 1;
+    }}
